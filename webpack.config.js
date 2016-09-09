1350f709f07972f44a06c05daf9f1f56c794d357
Tom Scallon <tssnowguy13@gmail.com>
2016-09-09 18:34:33 -0500

    Add .gitignore, create package.json, add webpack config and dependency

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const Webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/app.jsx',
+  output: {
+    path: './public',
+    filename: 'app.bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: [
+            'transform-react-display-name',
+            'transform-class-properties',
+            'transform-es2015-destructuring',
+            'transform-object-rest-spread'
+          ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'autoprefixer', 'sass']
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './src/style')],
+  },
+  plugins: [
+    new Webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [ '', '.js', '.jsx' ]
+  }
+};
